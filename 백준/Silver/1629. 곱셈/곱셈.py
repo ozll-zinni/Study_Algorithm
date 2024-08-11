32c1d266243e4c7762cc39b56a74b76bd1bae060
김예진 <107979181+yejineeeeee@users.noreply.github.com>
2024-08-11 19:46:43 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,16 +1,2 @@
-A, B, C = map(int, input().split())
-
-def modular_exponentiation(A, B, C):
-    result = 1
-    A = A % C
-    while B > 0:
-        if B % 2 == 1:
-            result = (result * A) % C
-        
-        B = B // 2
-        A = (A * A) % C
-    
-    return result
-
-result = modular_exponentiation(A, B, C)
-print(result)
+a, b, c = map(int, input().split())
+print(pow(a, b, c))
