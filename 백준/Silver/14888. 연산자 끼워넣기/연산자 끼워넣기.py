026c7b6a57fa444c7c68898d3930097f364220db
김예진 <107979181+yejineeeeee@users.noreply.github.com>
2024-08-11 14:57:06 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -10,29 +10,27 @@ mn = 1e9
 def dfs(n, temp) :
     global mx, mn
     
-    # 종료 조건
     if n == N-1:
         mx = max(temp, mx)
         mn = min(temp, mn)
         return
      
-    # 하부함수 호출
-    if operators[0] != 0 : # 덧셈하는 경우
+    if operators[0] != 0 :
         operators[0] -= 1
         dfs(n+1, temp + lst[n+1])
         operators[0] += 1 
 
-    if operators[1] != 0 : # 뺄셈하는 경우
+    if operators[1] != 0 :
         operators[1]-= 1
         dfs(n+1, temp - lst[n+1])
         operators[1] += 1
     
-    if operators[2] != 0 : # 곱셈하는 경우
+    if operators[2] != 0 :
         operators[2] -= 1
         dfs(n+1, temp * lst[n+1])
         operators[2] += 1
     
-    if operators[3] != 0 : #나눗셈하는 경우
+    if operators[3] != 0 :
         operators[3] -= 1
         dfs(n+1, int(temp/lst[n+1]))
         operators[3] += 1
