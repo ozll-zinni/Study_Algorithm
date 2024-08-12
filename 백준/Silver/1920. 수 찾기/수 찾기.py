e81f284af5d2c011c30c97b9f69609d20dd26c1e
김예진 <107979181+yejineeeeee@users.noreply.github.com>
2024-08-12 20:24:00 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,21 +1,9 @@
+import sys
+
 n = int(input())
-n_list = list(map(int, input().split()))
+n_list = set(map(int, input().split()))
 m = int(input())
 m_list = list(map(int, input().split()))
 
-def binary_search(arr, target):
-    left, right = 0, len(arr)-1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return 1
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid -1
-    return 0
-
-n_list.sort()
-for target in m_list:
-    print(binary_search(n_list, target))
+for target in (m_list):
+    print(1 if target in n_list else 0)
