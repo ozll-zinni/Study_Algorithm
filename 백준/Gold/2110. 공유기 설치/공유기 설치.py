c5133fd626205f717f40bf8a2acb885e109cbb1f
김예진 <107979181+yejineeeeee@users.noreply.github.com>
2024-08-14 17:22:10 +0900

    [Gold IV] Title: 공유기 설치, Time: 228 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,26 +1,27 @@
 import sys
 
-N, C = map(int, input().split())
-houses =  [int(sys.stdin.readline()) for _ in range(N)]
+n, c = map(int, sys.stdin.readline().split())
+houses = [int(sys.stdin.readline()) for _ in range(n)]
 houses.sort()
 
-left, right = 1, houses[-1] - houses[0]
-result = 0
-while left <= right:
-    mid = (left + right) //2
+def bi_search(n, c, houses):
+    # start = houses[0]
+    start = 1
+    end = houses[n-1] - houses[0]
 
-    prev_house = houses[0]
-    count = 1
-    
-    for i in range(1, len(houses)):
-        if houses[i] - prev_house >= mid:
-            prev_house = houses[i]
-            count += 1
-    
-    if count >= C:
-        result = mid
-        left = mid + 1
-    else:
-        right = mid - 1
+    while start <= end:
+        distance = (start + end) >> 1
 
-print(result)
+        point = houses[0]
+        cnt = 1
+
+        for i in range(1, len(houses)):
+            if houses[i] - point >= distance:
+                cnt += 1
+                point = houses[i]
+        if cnt < c:
+            end = distance - 1
+        elif cnt >= c:
+            start = distance + 1
+    return end
+print(bi_search(n, c, houses))
