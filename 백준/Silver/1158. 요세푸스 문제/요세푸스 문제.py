58b583f53f41f5df31e17d67cb69c44e1ded584e
김예진 <107979181+yejineeeeee@users.noreply.github.com>
2024-08-16 17:42:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 1792 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+n, k = map(int, input().split())
+output = [] # 뽑은 수 넣을 리스트
+queue = deque()
+
+for i in range(1, n+ 1):
+    queue.append(i)
+
+while queue:
+    for i in range(k-1):
+        queue.append(queue.popleft()) # 큐에서 k번째 뺀 수
+    output.append(queue.popleft()) 
+
+print("<", end = "")
+print(", ".join(map(str,output)), end="")
+print(">")
