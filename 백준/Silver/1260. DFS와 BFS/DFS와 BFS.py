92b0cd660932c4f923fa73d6e5904aa502798064
김예진 <107979181+yejineeeeee@users.noreply.github.com>
2024-08-19 09:49:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 60 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -8,44 +8,34 @@ for _ in range(m):
     graph[a].append(b)
     graph[b].append(a)
 
-# 작은 번호부터 방문하도록 각 정점의 인접 리스트 정렬
-for g in graph:
-    g.sort()
-
+for g in graph: # 작은 번호부터 방문하기 위해 각 정점의 인접 리스트 정렬
+    g.sort()  # 여기에 괄호를 추가해야 리스트가 정렬됩니다.
 
 def dfs(v):
-    # 현재 정점 v를 방문했음을 표시하고 결과 리스트에 추가
-    dfs_visited[v] = True
+    dfs_visited[v] = True  # 정점 v를 방문한 경우 visited에 추가
     dfs_result.append(v)
 
-    # 현재 정점에 연결된 인접 정점들을 하나씩 방문
-    for next_node in graph[v]:
-        if not dfs_visited[next_node]:  # 아직 방문하지 않은 정점인 경우
-            dfs(next_node)  # 해당 정점을 시작점으로 다시 DFS 수행
-
+    for next_node in graph[v]:  # 현재 정점에 연결된 인접 정점들 하나씩 방문
+        if not dfs_visited[next_node]:  # 아직 방문 안 한 정점이면
+            dfs(next_node)  # 해당 정점을 시작점으로 dfs 수행
 
 def bfs(start):
-    # 방문한 정점을 체크하기 위한 리스트
-    bfs_visited = [False] * (n+1)
-    # 시작점 방문 체크
-    bfs_visited[start] = True
+    bfs_visited = [False] * (n+1)  # 방문한 정점 체크하기 위한 리스트
     bfs_result = []
 
-    # BFS를 위한 큐 생성, 시작점을 큐에 추가
-    queue = deque([start])
+    bfs_visited[start] = True  # 시작점 방문 체크    
+    queue = deque([start])  # bfs를 위한 큐 생성, 시작점을 큐에 추가
+
     while queue:
-        # 큐의 맨 앞에 있는 정점을 꺼내서 방문
-        visit = queue.popleft()
+        visit = queue.popleft()  # 큐의 맨 앞에 있는 정점을 꺼내서 방문
         bfs_result.append(visit)
 
-        # 현재 정점에 연결된 인접 정점들을 하나씩 방문
-        for next_node in graph[visit]:
-            if not bfs_visited[next_node]:  # 아직 방문하지 않은 정점인 경우
-                bfs_visited[next_node] = True  # 해당 정점을 방문 체크하고 큐에 추가
+        for next_node in graph[visit]:  # 현재 정점에 연결된 인접 정점들을 하나씩 방문
+            if not bfs_visited[next_node]:  # 아직 방문 안 한 정점이면
+                bfs_visited[next_node] = True  # 해당 정점을 방문하고 큐에 추가
                 queue.append(next_node)
 
     return bfs_result
-
 
 dfs_visited = [False] * (n+1)
 dfs_result = []
