ac83215fe6a11bb06886fe3c51806af7a9c3b2d8
김예진 <107979181+ozll-zinni@users.noreply.github.com>
2025-01-02 23:52:46 +0900

    [level 2] Title: 카펫, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,9 @@
+function solution(brown, yellow) {
+    for (var y = 3; y <= (brown + yellow)/y; y++){
+        var x = Math.floor((brown + yellow)/y);
+        if((x-2)*(y-2) === yellow) {
+            break;
+        }
+    }
+    return [x,y];
+}
