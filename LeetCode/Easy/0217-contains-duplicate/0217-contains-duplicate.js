d3ab5be0c1aa439936a67dbda7a4fee7ec4d90b8
김예진 <107979181+ozll-zinni@users.noreply.github.com>
2025-01-02 13:46:23 +0900

    Time: 9 ms (89.32%), Space: 63.4 MB (21.53%) - LeetHub

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let seen = new Set();
+
+    for (let num of nums){
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    }
+    return false
+};
