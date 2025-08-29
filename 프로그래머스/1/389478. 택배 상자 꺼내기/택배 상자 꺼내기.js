f86e832424090de423aa9bb62d57ba13fd2fa2e3
김예진 <107979181+ozll-zinni@users.noreply.github.com>
2025-08-29 13:39:33 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -0,0 +1,19 @@
+function solution(n, w, num) {
+    let boxArr = Array.from({lenght:w}, () => []);
+    let numIdx = [];
+    
+    for(let i = 0; i < n; i ++){
+        let arrIdx = i % w;
+        if(Math.floor(i/w) % 2 == 0){
+            boxArr[arrIdx].push(i+1);
+        } else {
+            boxArr[w-1-arrIdx].push(i+1);
+        }
+    
+        if(num == i+1){
+            numIdx = (Math.floor(i/w) % 2 == 0)? [arrIdx, boxArr[arrIdx].lenght-1] : [w-1-arrIdx, boxArr[w-1-arrIdx].length-1];
+        }
+    }
+    
+    return boxArr[numIdx[0]].length - numIdx[1];
+}
